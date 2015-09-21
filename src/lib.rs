0375cbaf622b8759772e5c3efab932f2c64664e4
msiglreith <msiglreith@users.noreply.github.com>
2015-09-21 17:51:42 +0200

    Implement AsRef and AsMut traits and mark methods as inline

--- src/lib.rs
+++ src/lib.rs
@@ -12,46 +12,59 @@ pub struct ComPtr<T> {
 }
 
 impl<T> ComPtr<T> {
+    #[inline]
     pub fn new(ptr: *mut T) -> ComPtr<T> {
         ComPtr { pointer: ptr }
     }
 
+    #[inline]
     pub fn as_ptr(&self) -> *const T {
         self.pointer
     }
 
+    #[inline]
     pub fn as_mut_ptr(&self) -> *mut T {
         self.pointer
     }
 
-    pub fn get_ref(&mut self) -> & *mut T {
-        &self.pointer
-    }
-
-    pub fn get_mut_ref(&mut self) -> &mut *mut T {
-        &mut self.pointer
-    }
-
+    #[inline]
     pub fn is_null(&self) -> bool {
         self.pointer.is_null()
     }
 }
 
 impl<T> fmt::Pointer for ComPtr<T> {
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Pointer::fmt(&self.pointer, f)
     }
 }
 
 impl<T> fmt::Debug for ComPtr<T> {
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Pointer::fmt(self, f)
+    }
+}
+
+impl<T> AsRef<*mut T> for ComPtr<T> {
+    #[inline]
+    fn as_ref(&self) -> & *mut T {
+        &self.pointer
+    }
+}
+
+impl<T> AsMut<*mut T> for ComPtr<T> {
+    #[inline]
+    fn as_mut(&mut self) -> &mut *mut T {
+        &mut self.pointer
     }
 }
 
 impl<T> Deref for ComPtr<T> {
     type Target = T;
 
+    #[inline]
     fn deref<'a>(&'a self) -> &'a T {
         assert!(!self.is_null(), "can't deref null-ptr!");
         unsafe { &*self.pointer }
@@ -59,6 +72,7 @@ impl<T> Deref for ComPtr<T> {
 }
 
 impl<T> DerefMut for ComPtr<T> {
+    #[inline]
     fn deref_mut<'a>(&'a mut self) -> &'a mut T {
         assert!(!self.is_null(), "can't deref null-ptr!");
         unsafe { &mut *self.pointer }
@@ -66,6 +80,7 @@ impl<T> DerefMut for ComPtr<T> {
 }
 
 impl<T> Clone for ComPtr<T> {
+    #[inline]
     fn clone(&self) -> ComPtr<T> {
         if !self.is_null() {
             unsafe { (*(self.pointer as *mut IUnknown)).AddRef(); }
@@ -76,6 +91,7 @@ impl<T> Clone for ComPtr<T> {
 }
 
 impl<T> Drop for ComPtr<T> {
+    #[inline]
     fn drop(&mut self) {
         if !self.is_null() {
             unsafe { (*(self.pointer as *mut IUnknown)).Release(); }
